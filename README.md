dd5bbdee1de00946d61348686edbffcbcd570828
pathompat <mungonepiece@gmail.com>
2017-02-18 23:57:49 +0700

    Delete polls app

--- README.md
+++
@@ -1,2 +0,0 @@
-# quiz
-# Test Second Commit
--- mysite/mysite/views.py
+++
@@ -1,7 +0,0 @@
-from django.template import loader
-from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect, HttpResponse ,Http404
-
-def index(request):
-    return HttpResponse('<h1>Fill your name.</h1><br><input type="text" name="name"><br><br><input type="submit">'
-) 
