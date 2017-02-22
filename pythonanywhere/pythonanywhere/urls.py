5b7054f7f8635851402ad66e6a216445e232f693
pathompat <mungonepiece@gmail.com>
2017-02-23 02:15:38 +0700

    Use CSS & Edit Urls

--- pythonanywhere/pythonanywhere/urls.py
+++ pythonanywhere/pythonanywhere/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
     url(r'^quiz/', include('quiz.urls')),
+    url(r'^$', views.index, name='index'),
     url(r'^admin/', admin.site.urls),
 ]
